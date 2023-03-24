8ff04c45dfa7b307e222335781b212d338afc2e5
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-03-24 19:35:16 +0200

    Create MovieDetalis.styled.jsx

---
+++ src/components/MovieDetails/MovieDetalis.styled.jsx
@@ -0,0 +1,45 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const GoBackBtn = styled(NavLink)`
+  display: inline-block;
+  text-align: center;
+  padding: 8px 16px;
+
+  text-decoration: none;
+  border-radius: 4px;
+
+  background-color: grey;
+  color: white;
+  margin-bottom: 16px;
+`;
+
+export const MovieInfoWrapper = styled.div`
+  padding: 20px;
+`;
+
+export const MovieDetailsBox = styled.div`
+  display: flex;
+  gap: 20px;
+
+  img {
+    width: 200px;
+  }
+
+  h2 {
+    margin-bottom: 8px;
+  }
+
+  h3 {
+    margin-bottom: 8px;
+  }
+
+  span {
+    display: block;
+    margin-bottom: 24px;
+  }
+
+  p {
+    margin-bottom: 16px;
+  }
+`;
